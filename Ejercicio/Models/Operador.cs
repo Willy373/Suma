955f5713ebb593f61206170fd783f2fd037614b3
william <ofeliavm69@gmail.com>
2024-05-15 19:15:15 -0400

    Creando el controlador y la vista

--- Ejercicio/Models/Operador.cs
+++ Ejercicio/Models/Operador.cs
@@ -2,5 +2,9 @@
 {
     public class Operador
     {
+        //prop + tabulador 
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Total { get; set; }
     }
 }
